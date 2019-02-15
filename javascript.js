efcb0bfd186b29e267e31dc18a985cbe9409ffe0
Millcent <47664149+Millcent@users.noreply.github.com>
2019-02-15 15:21:09 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,40 @@
+function num(a,b) {
+    if (a==65 || a + b == 65)
+        {
+        return true;
+        }
+    return false;
+ }
+ console.log(num(13, 65));
+ 
+ 
+ var sidea = 7;
+ var sideb = 8;
+ var sidec = 9;
+ var perimeter = (sidea + sideb + sidec / 2);
+ var area = Math.sqrt(perimeter * ((perimeter - sidea) * (perimeter - sideb) * (perimeter -sidec)))
+ console.log(area);
+  
+
+function max(a,b,c) {
+  if (a > b && a > c) {
+    return a;
+  }
+  else if (b > a && b > c){
+    return b;
+  }
+  else {
+  
+  }
+  
+}max(1,8,6);
+
+
+
+    
+}
+    
+  
+  
+  
+  
